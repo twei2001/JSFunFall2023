ea2234f377edaa30e75b601a4d695825f3a7f60f
matinaspatsos <matinaspatsos@gmail.com>
2023-11-05 08:52:35 -0500

    Solutions to cloning objects exercises

---
+++ solutions/14-cloning-objects/01-change-email.solutions.js
@@ -0,0 +1,21 @@
+const user = { id: 1234, username: "lolcatz1", email: "lolcatz1@gmail.com" }; // e.g.
+const newEmail = "purrfect@gmail.com"; // e.g.
+
+/**
+ * Create an object called "newUser". It should be equal to "user" with this small change:
+ * The email address should be equal to "newEmail" (e.g. "purrfect@gmail.com").
+ * @example console.log(newUser);
+ * // { id: 1234, username: "lolcatz1", email: "purrfect@gmail.com" };
+ *
+ * You should use the spread operator to shallow copy the original object.
+ *
+ * Your answer should still work when "user" and "newEmail" are equal to different values.
+ */
+
+// WRITE YOUR ANSWER BELOW THIS LINE
+
+const newUser = { ...user, email: newEmail };
+
+// Here is how you could have solved this problem with "structuredClone":
+const newUser = structuredClone(user);
+newUser.email = newEmail;
